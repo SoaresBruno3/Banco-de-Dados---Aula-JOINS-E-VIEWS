36a4e8b9ca144ef1d05e7d8a446d423b17d1d239
SoaresBruno3 <160656274+SoaresBruno3@users.noreply.github.com>
2025-03-31 16:21:35 -0300

    Update Script (Aula JOINS e VIEWS).sql

--- Script (Aula JOINS e VIEWS).sql
+++ Script (Aula JOINS e VIEWS).sql
@@ -1,5 +1,7 @@
+-- Questão 1 Crie uma relação a partir da união das tabelas student e takes --
 SELECT * FROM student JOIN takes on student.ID = takes.ID ;
 
+--Questão 2. Contar a quantidade de cursos realizados pelos alunos do departamento de Civil Eng. --
 SELECT 
     st.ID,
     st.name,
@@ -10,9 +12,11 @@ LEFT OUTER JOIN
     takes ON st.ID = takes.ID
     GROUP BY 
     st.ID, st.name
+--Ordenar de maneira descendente a quantidade de cursos associada aos alunos. --
 	ORDER BY 
     qtd_cursos DESC;
 
+-- Questão 3. Criar uma view chamada 'civil_eng_students' a partir da relação construída na Questão 2. --
 CREATE VIEW civil_eng_students as
 SELECT
 	st.ID,
@@ -25,6 +29,7 @@ LEFT OUTER JOIN
 	GROUP BY 
 	st.ID, st.name;
 
+--Acessando a View--
 SELECT * FROM civil_eng_students
 ORDER BY qtd_cursos DESC;
 
